02b1eb78b72518a08b684466802f060059d90c30
Haripriya Tripathi <197859487+haripriyatripathi@users.noreply.github.com>
2026-06-29 00:20:30 +0530

    Time: 0 ms (100.00%) | Memory: 12.4 MB (59.90%) - LeetSync

---
+++ 2195-time-needed-to-buy-tickets/time-needed-to-buy-tickets.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def timeRequiredToBuy(self, tickets, k):
+        # total time taken
+        time = 0
+
+        for i in range(len(tickets)):
+            if i <= k:
+                time += min(tickets[i], tickets[k])
+            else:
+                time += min(tickets[i], tickets[k] - 1)
+
+        return time
